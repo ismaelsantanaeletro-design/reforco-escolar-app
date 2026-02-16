946716eb09309f6f1282a61bff03706cc20224fe
ismaelsantanaeletro-design <ismaelsantana.eletro@gmail.com>
2026-02-16 11:28:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,86 @@ const perguntas = {
   "1ano": {
 
     matematica: [
+
       {
         pergunta: "Quanto é 1 + 1?",
         opcoes: ["1", "2", "3"],
         correta: "2"
+      },
+
+      {
+        pergunta: "Quanto é 2 + 2?",
+        opcoes: ["3", "4", "5"],
+        correta: "4"
       }
+
+    ],
+
+    portugues: [
+
+      {
+        pergunta: "Qual é a primeira letra do alfabeto?",
+        opcoes: ["A", "B", "C"],
+        correta: "A"
+      },
+
+      {
+        pergunta: "Qual é o plural de casa?",
+        opcoes: ["casas", "casos", "casa"],
+        correta: "casas"
+      }
+
+    ],
+
+    ciencias: [
+
+      {
+        pergunta: "O sol é uma?",
+        opcoes: ["estrela", "planeta", "lua"],
+        correta: "estrela"
+      },
+
+      {
+        pergunta: "Precisamos de qual para respirar?",
+        opcoes: ["oxigênio", "água", "terra"],
+        correta: "oxigênio"
+      }
+
+    ],
+
+    historia: [
+
+      {
+        pergunta: "Quem descobriu o Brasil?",
+        opcoes: ["Pedro Álvares Cabral", "Dom Pedro", "Tiradentes"],
+        correta: "Pedro Álvares Cabral"
+      },
+
+      {
+        pergunta: "O Brasil fica em qual continente?",
+        opcoes: ["América", "Europa", "Ásia"],
+        correta: "América"
+      }
+
+    ],
+
+    geografia: [
+
+      {
+        pergunta: "Onde moramos?",
+        opcoes: ["Brasil", "EUA", "Japão"],
+        correta: "Brasil"
+      },
+
+      {
+        pergunta: "Qual é a capital do Brasil?",
+        opcoes: ["Brasília", "Rio", "São Paulo"],
+        correta: "Brasília"
+      }
+
+    ]
+
+  }
     ],
 
     portugues: [
